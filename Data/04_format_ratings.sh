94e81e1e98f4cae1fa0626daa58b40e6a09f19fb
DereckTav <derecktaverne@gmail.com>
2025-06-17 16:43:36 -0400

    New Stuff

--- Data/04_format_ratings.sh
+++ Data/04_format_ratings.sh
@@ -1,11 +1,19 @@
+
+start_date=$(date -d "2021-01-23T00:00:00Z" +%s000)
+end_date=$(date -d "2021-07-31T23:59:59Z" +%s000)
+
 for i in $(seq 0 19)
 do 
+    if ((i == 0))
+    then 
+        unzip -p ratings-000${i}.zip | awk -F "\t" 'NR==1' >> ratings.tsv
+    fi
+
     if ((i < 10)) 
     then 
+        unzip -p ratings-0000${i}.zip | awk -F "\t" -v start="$start_date" -v end="$end_date" '($3 >= start && $3 <= end)' >> ratings.tsv
 
-    unzip ratings-0000${i}.zip
     else
-    unzip ratings-000${i}.zip
-
+        unzip -p ratings-000${i}.zip | awk -F "\t" -v start="$start_date" -v end="$end_date" '($3 >= start && $3 <= end)' >> ratings.tsv
     fi
 done
