034a84dcc93232f5c8a0ff6c1b8844a1fdc22a52
Dereck Taverne <derecktaverne@gmail.com>
2025-06-17 16:34:59 -0400

    Update 04_format_ratings.sh

--- Data/04_format_ratings.sh
+++ Data/04_format_ratings.sh
@@ -4,6 +4,11 @@ end_date=$(date -d "2021-07-31T23:59:59Z" +%s000)
 
 for i in $(seq 0 19)
 do 
+    if ((i == 0))
+    then 
+        unzip -p ratings-000${i}.zip | awk -F "\t" 'NR==1' >> ratings.tsv
+    fi
+
     if ((i < 10)) 
     then 
         unzip -p ratings-0000${i}.zip | awk -F "\t" -v start="$start_date" -v end="$end_date" '($3 >= start && $3 <= end)' >> ratings.tsv
